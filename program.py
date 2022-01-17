850b9cb999a82d060db24b20fe88e87f17a1fbdf
NOOB4FUN <90191235+NOOB4FUN@users.noreply.github.com>
2022-01-17 13:28:18 +0100

    yes

---
+++ program.py
@@ -0,0 +1,13 @@
+grisar = input("hur många grisar har du? ")
+fiskar = input("hur många fiskar har du? ")
+
+if grisar < fiskar:
+    print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+if grisar > fiskar:
+    print("haha cringe du suger")
+
+elif grisar == fiskar:
+    print("haha ganska cringe döda några och försök igen")
+
+
